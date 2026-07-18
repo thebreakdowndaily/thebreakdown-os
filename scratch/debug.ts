4ab3c72c3fab25ed148f1ca7a44f8e8e344fd7ce
Ntn <hello@breakdown.in>
2026-07-18 08:08:23 +0530

    chore: remove debug script

--- scratch/debug.ts
+++
@@ -1,5 +0,0 @@
-import { RelationshipGraphPlugin } from "../plugins/relationship-graph/index";
-
-console.log("RelationshipGraphPlugin:", RelationshipGraphPlugin);
-console.log("manifest:", RelationshipGraphPlugin.manifest);
-console.log("renderer:", RelationshipGraphPlugin.renderer);
